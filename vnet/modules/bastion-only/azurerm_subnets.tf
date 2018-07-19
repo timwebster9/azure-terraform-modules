77048cd022e702bdc1eea543585c2bc45b0ca760
timwebster9 <tim.webster@gmail.com>
2018-07-19 20:05:57 +0100

    fix security group

--- vnet/modules/bastion-only/azurerm_subnets.tf
+++ vnet/modules/bastion-only/azurerm_subnets.tf
@@ -2,7 +2,6 @@ data "azurerm_subnet" "bastion_subnet" {
   name                      = "${var.bastion_subnet_name}"
   resource_group_name       = "${data.azurerm_resource_group.rg.name}"
   virtual_network_name      = "${data.azurerm_virtual_network.vnet.name}"
-  network_security_group_id = "${azurerm_network_security_group.bastion_sg.id}"
 }
 
 data "azurerm_subnet" "jenkins_subnet" {
